41adf91b5a4971506443b8b32d1657811915fe49
Faruk Spahic <farukspahictz@gmail.com>
2023-07-13 15:45:57 +0200

    Added pawn positions

--- frontend/src/pages/Board.tsx
+++ frontend/src/pages/Board.tsx
@@ -7,9 +7,17 @@ import {
   blueZone,
   path,
 } from "../constants/constants";
+import { Player } from "./SinglePlayer";
 
-const Board = () => {
+interface Props {
+  currentPlayerTurn: string;
+  players: Player[];
+}
+
+const Board = ({ currentPlayerTurn, players }: Props) => {
   const [cells, setCells] = useState<number[]>([]);
+
+  console.log(cells);
 
   useEffect(() => {
     const createGrid = () => {
@@ -28,33 +36,73 @@ const Board = () => {
   return (
     <div className="grid justify-center h-[100vh] items-center">
       <div className="board h-[650px]">
-        {cells.map((cell) => (
-          <div
-            key={cell}
-            className={classNames("", {
-              "border border-gray-300": path.includes(cell + 1),
-              "bg-red-500 rounded-full border-none":
-                redZone.playerZones.includes(cell + 1),
+        {cells.map((cell, index) => {
+          const hasPawnOnCell = players.some(
+            (player) =>
+              player.pawnOnePosition === cell + 1 ||
+              player.pawnTwoPosition === cell + 1 ||
+              player.pawnThreePosition === cell + 1 ||
+              player.pawnFourPosition === cell + 1
+          );
 
-              "bg-red-500 border-none": redZone.border.includes(cell + 1),
-              "bg-red-500 border": redZone.finalZones.includes(cell + 1),
-              "bg-green-600 border-none": greenZone.border.includes(cell + 1),
-              "bg-green-600 rounded-full": greenZone.playerZones.includes(
-                cell + 1
-              ),
-              "bg-green-600 border": greenZone.finalZones.includes(cell + 1),
-              "bg-blue-600 border-none": blueZone.border.includes(cell + 1),
-              "bg-blue-600 rounded-full": blueZone.playerZones.includes(
-                cell + 1
-              ),
-              "bg-blue-600 border": blueZone.finalZones.includes(cell + 1),
-              "bg-yellow-400 border-none": yellowZone.border.includes(cell + 1),
-              "bg-yellow-400 rounded-full relative":
-                yellowZone.playerZones.includes(cell + 1),
-              "bg-yellow-400 border": yellowZone.finalZones.includes(cell + 1),
-            })}
-          ></div>
-        ))}
+          const pawnColor = players.find(
+            (player) =>
+              player.pawnOnePosition === cell + 1 ||
+              player.pawnTwoPosition === cell + 1 ||
+              player.pawnThreePosition === cell + 1 ||
+              player.pawnFourPosition === cell + 1
+          )?.color;
+
+          return (
+            <div
+              key={cell}
+              className={classNames(
+                "relative flex justify-center items-center",
+                {
+                  "border border-gray-300": path.includes(cell + 1),
+                  "bg-red-600 rounded-full border-none mb-1 mx-1":
+                    redZone.playerZones.includes(cell + 1),
+                  "bg-red-600 border-none": redZone.border.includes(cell + 1),
+                  "bg-red-600 border": redZone.finalZones.includes(cell + 1),
+                  "bg-green-600 border-none": greenZone.border.includes(
+                    cell + 1
+                  ),
+                  "bg-green-600 rounded-full mb-1 mx-1":
+                    greenZone.playerZones.includes(cell + 1),
+                  "bg-green-600 border": greenZone.finalZones.includes(
+                    cell + 1
+                  ),
+                  "bg-blue-600 border-none": blueZone.border.includes(cell + 1),
+                  "bg-blue-600 rounded-full mb-1 mx-1":
+                    blueZone.playerZones.includes(cell + 1),
+                  "bg-blue-600 border": blueZone.finalZones.includes(cell + 1),
+                  "bg-yellow-400 border-none": yellowZone.border.includes(
+                    cell + 1
+                  ),
+                  "bg-yellow-400 rounded-full mb-1 mx-1":
+                    yellowZone.playerZones.includes(cell + 1),
+                  "bg-yellow-400 border": yellowZone.finalZones.includes(
+                    cell + 1
+                  ),
+                }
+              )}
+            >
+              {hasPawnOnCell && (
+                <div
+                  className={classNames(
+                    "absolute h-[2rem] w-[2rem] border-4 border-black rounded-full",
+                    {
+                      "bg-red-400": pawnColor === "red",
+                      "bg-blue-400": pawnColor === "blue",
+                      "bg-green-400": pawnColor === "green",
+                      "bg-yellow-400": pawnColor === "yellow",
+                    }
+                  )}
+                ></div>
+              )}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- frontend/src/pages/SinglePlayer.tsx
+++ frontend/src/pages/SinglePlayer.tsx
@@ -1,27 +1,85 @@
 import React, { useEffect, useState } from "react";
 import Board from "./Board";
+import { IPlayer } from "../types/types";
+
+export class Player {
+  color: null | string = null;
+  pawnOnePosition: number | null = null;
+  pawnTwoPosition: number | null = null;
+  pawnThreePosition: number | null = null;
+  pawnFourPosition: number | null = null;
+
+  constructor({ color }: IPlayer) {
+    if (!this.color) {
+      this.color = color;
+
+      switch (color) {
+        case "red":
+          this.pawnOnePosition = 33;
+          this.pawnTwoPosition = 34;
+          this.pawnThreePosition = 48;
+          this.pawnFourPosition = 49;
+          break;
+        case "green":
+          this.pawnOnePosition = 42;
+          this.pawnTwoPosition = 43;
+          this.pawnThreePosition = 57;
+          this.pawnFourPosition = 58;
+          break;
+        case "blue":
+          this.pawnOnePosition = 168;
+          this.pawnTwoPosition = 169;
+          this.pawnThreePosition = 183;
+          this.pawnFourPosition = 184;
+          break;
+        default:
+          this.pawnOnePosition = 177;
+          this.pawnTwoPosition = 178;
+          this.pawnThreePosition = 192;
+          this.pawnFourPosition = 193;
+          break;
+      }
+    }
+  }
+}
 
 const SinglePlayer = () => {
   const [randomNum, setRandomNum] = useState<null | number>(null);
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [currentPlayerTurn, setCurrentPlayerTurn] = useState("");
 
-  useEffect(() => {
-    const initGame = () => {};
+  console.log(players);
 
-    initGame();
-  }, []);
-
-  const throwDice = () => {
+  const handleDiceThrow = () => {
     const randomNum = Math.floor(Math.random() * 6 + 1);
+
+    // const currentPlayerIndex = players.findIndex(
+    //   (player) => player.color === currentPlayerTurn
+    // );
+
+    // players[currentPlayerIndex] =
 
     setRandomNum(randomNum);
   };
 
+  useEffect(() => {
+    const initGame = () => {
+      const player = new Player({ color: "red" });
+      const computer = new Player({ color: "blue" });
+
+      setCurrentPlayerTurn("red");
+      setPlayers([player, computer]);
+    };
+
+    initGame();
+  }, []);
+
   return (
     <section>
-      <div className="absolute flex space-x-4 items-center bottom-10 left-10">
+      <div className="absolute flex items-center space-x-4 bottom-10 left-10">
         <button
-          onClick={() => throwDice()}
-          className=" bg-blue-500 text-white p-2 rounded-md hover:bg-blue-400"
+          onClick={() => handleDiceThrow()}
+          className="p-2 text-white bg-blue-500 rounded-md hover:bg-blue-400"
         >
           Throw
         </button>
@@ -29,7 +87,7 @@ const SinglePlayer = () => {
         <span className="text-2xl">{randomNum}</span>
       </div>
 
-      <Board />
+      <Board players={players} currentPlayerTurn={currentPlayerTurn} />
     </section>
   );
 };
