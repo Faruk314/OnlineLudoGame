395e18b18602de825ecffc3d079719b923b6ffa6
Faruk Spahic <farukspahictz@gmail.com>
2023-07-16 19:29:22 +0200

    Added chose number of players modal

---
+++ frontend/src/modals/Local.tsx
@@ -0,0 +1,84 @@
+import React, { useContext, useState } from "react";
+import { GameContext } from "../context/GameContext";
+import ChoseColors from "./ChoseColors";
+
+interface Props {
+  setOpenLocal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Local = ({ setOpenLocal }: Props) => {
+  const { setChosenPlayers, chosenPlayers } = useContext(GameContext);
+  const [showChoseColors, setShowChoseColors] = useState(false);
+
+  console.log(chosenPlayers);
+
+  return (
+    <div className="fixed top-0 bottom-0 left-0 right-0 z-30 flex flex-col items-center justify-center text-center bg-[rgb(0,0,0,0.5)]">
+      {!showChoseColors && (
+        <div className="relative z-40 flex flex-col items-center justify-center px-[3rem] py-3 mx-2 space-y-4 bg-white rounded-md shadow-xl">
+          <button
+            onClick={() => {
+              setChosenPlayers(0);
+              setOpenLocal(false);
+            }}
+            className="flex absolute top-2 right-2 items-center border border-black hover:bg-red-600 justify-center w-[2rem] h-[2rem]  text-white bg-red-500 rounded-md"
+          >
+            X
+          </button>
+
+          <div className="flex flex-col py-6 space-y-2">
+            <div className="flex items-center space-x-2 text-2xl">
+              <input
+                onChange={(e) => setChosenPlayers(parseInt(e.target.value))}
+                className="w-4 h-4 border border-black rounded-full appearance-none cursor-pointer checked:bg-red-500"
+                type="radio"
+                id="2"
+                name="player"
+                value={2}
+              />
+              <label htmlFor="easy">Two players</label>
+            </div>
+
+            <div className="flex items-center space-x-2 text-2xl">
+              <input
+                onChange={(e) => setChosenPlayers(parseInt(e.target.value))}
+                className="w-4 h-4 border border-black rounded-full appearance-none cursor-pointer checked:bg-red-500"
+                type="radio"
+                id="3"
+                name="player"
+                value={3}
+              />
+              <label htmlFor="hard">Three players</label>
+            </div>
+
+            <div className="flex items-center space-x-2 text-2xl">
+              <input
+                onChange={(e) => setChosenPlayers(parseInt(e.target.value))}
+                className="w-4 h-4 border border-black rounded-full appearance-none cursor-pointer checked:bg-red-500"
+                type="radio"
+                id="4"
+                name="player"
+                value={4}
+              />
+              <label htmlFor="hard">Four players</label>
+            </div>
+          </div>
+
+          <button
+            disabled={chosenPlayers === 0 ? true : false}
+            onClick={() => {
+              setShowChoseColors(true);
+            }}
+            className="px-5 py-2 font-bold text-white bg-red-500 border border-black rounded-lg disabled:bg-gray-400 hover:bg-red-600 disabled:text-gray-200"
+          >
+            Next
+          </button>
+        </div>
+      )}
+
+      {showChoseColors && <ChoseColors />}
+    </div>
+  );
+};
+
+export default Local;
