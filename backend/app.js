9b9a5f887098aac3376491d80f8b0e9159b56180
Faruk Spahic <farukspahictz@gmail.com>
2023-07-17 20:45:14 +0200

    Added game routes

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import errorHandler from "./utils/error.js";
 import authRoutes from "./routes/auth.js";
+import gameRoutes from "./routes/game.js";
 config();
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/game", gameRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("server is listening on", process.env.PORT);
